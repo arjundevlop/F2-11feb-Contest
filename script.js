b7f1138d5c959e25ae02e279117cf1751acb7c6e
arjundevlop <109807311+arjundevlop@users.noreply.github.com>
2023-02-11 18:23:17 +0530

    updated script.js

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  const newArray = arr.map(myFunction);
+    
+    function myFunction(value){
+        if(value.marks>50)
+        {
+            console.log(value);
+        }
+
+    }
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(frEachfun);
+    function frEachfun(value){
+        if(value.marks>50)
+        {
+            console.log(value);
+        }
+
+    }
 }
 
 function addData() {
   //Write your code here, just console.log
+  var obj = {id:4,name:"susan",age:"20",marks:45};
+    arr.push(obj);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  const array2 = arr.filter(student=>student.marks>=50);
+     console.log(array2);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+        { id: 5, name: "rahul", age: "22", marks: 70 },
+        { id: 6, name: "rohan", age: "21", marks: 75 },
+        { id: 7, name: "arjun", age: "23", marks: 86 },
+      ];
+      let mergedArray = arr.concat(arr2);
+      console.log(mergedArray);
 }
